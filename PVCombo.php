c81bd047268f6decf8c9bd772070593de49bb5e8
Matthew Murphy <matthew.e.murphy@phila.gov>
2016-03-20 09:06:13 -0400

    ksort

--- PVCombo.php
+++ PVCombo.php
@@ -226,7 +226,7 @@ class PVCombo
         foreach ($object as $item) {
             $tmp[$item->$key] = (string) $item->$value;
         }
-        sort($tmp);
+        ksort($tmp);
         return self::setComboData($tmp);
     }
 }
